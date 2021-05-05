be1f88f2dd090748f84b3e81c77ab0cd68855698
techydinesh <dude15to45@gmail.com>
2021-05-05 09:10:35 +0530

    Initial Commit

---
+++ app/src/main/java/com/techydinesh/quicksaver/interfaces/UserListInterface.java
@@ -0,0 +1,8 @@
+package com.techydinesh.quicksaver.interfaces;
+
+
+import com.techydinesh.quicksaver.model.story.TrayModel;
+
+public interface UserListInterface {
+    void userListClick(int position, TrayModel trayModel);
+}
